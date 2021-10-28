ac00ab27af9c8175f6c42a56b2a43d68eca22314
Vishwajeet <vkchintu53@gmail.com>
2021-10-29 02:08:29 +0530

    Adding Newsletter and Footer

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 
+import Join from "../Components/Join_our_newsletter/join"
+import Foot from "../Components/Footer/foot"
+
 const HomePage = () => {
 	return (
-		<div className="absolute inset-0 grid bg-pink-500 place-content-center">
-			<p className="text-lg font-bold text-white sm:text-4xl">
-				Hello peter
-			</p>
+		<div className="space-y-4">
+			
+			<Join/>
+			<Foot/>
 		</div>
 	)
 }
