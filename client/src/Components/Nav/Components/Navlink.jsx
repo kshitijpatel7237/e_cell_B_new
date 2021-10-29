f134700365191f0c951d2dbcea5de6b4857f5836
abhinav2401 <abhinavgoel2576@gmail.com>
2021-10-29 17:23:49 +0530

    fixed a minor issue

--- client/src/Components/Nav/Components/Navlink.jsx
+++ client/src/Components/Nav/Components/Navlink.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 function Navlink(props){
     return (
         <div>
-        <Link to="/" className="px-3 py-2 flex items-center text-xl font-semibold mt-2 text-custom-muted hover:opacity-75 ml-2">{props.text}</Link>
+        <Link to="/" className="px-3 py-2 flex items-center text-xl mt-2 text-custom-muted hover:opacity-75 ml-2">{props.text}</Link>
        </div>
     )
 }
